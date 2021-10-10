da25d11b396fef373695c3793a9f91ea95b66e95
georgiosan <georgiosanagnostou@hotmail.com>
2021-10-10 03:34:53 +0300

    Adding gsap animations

---
+++ src/utils/animation.js
@@ -0,0 +1,19 @@
+import { gsap } from 'gsap';
+
+export default function animateGsap(
+  el,
+  el2 = el,
+  el3 = el2,
+  el4 = el3,
+  el5 = el4,
+  el6 = el5
+) {
+  const timeline = gsap.timeline({ defaults: { duration: 0.5, opacity: 0 } });
+  timeline
+    .from(`${el}`, { ease: 'Power2.easeIn' })
+    .from(`${el2}`, { duration: 0.7, y: -100, ease: 'Power1.easeOut' })
+    .from(`${el3}`, { x: -300, stagger: 0.3 })
+    .from(`${el4}`, { backgroundPosition: '200px 0px', stagger: 0.15 }, '-=0.5')
+    .from(`${el5}`, { stagger: 0.15 }, '-=1')
+    .from(`${el6}`, { backgroundPosition: '0px 100px' }, '-=2');
+}
