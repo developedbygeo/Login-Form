51857e38f3cd02035caee95c6298c28c04ed5e2d
georgiosan <georgiosanagnostou@hotmail.com>
2021-10-10 03:35:11 +0300

    Adding the base js file

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './styles/style.scss';
+import fontawesome from '../node_modules/@fortawesome/fontawesome-free/js/all';
+import introAnimations from './init/eventListeners';
+
+introAnimations();
