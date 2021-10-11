913aed53396f2fa716ea17832ab193a7c7fefccb
georgiosan <georgiosanagnostou@hotmail.com>
2021-10-11 03:34:04 +0300

    Added validityCheck func with the respective eventlisteners / functions

--- src/init/eventListeners.js
+++ src/init/eventListeners.js
@@ -1,8 +1,22 @@
 import animateGsap from '../utils/animation';
+import { validate, validateSelect, preventSubmission } from './validate';
 
-export default function introAnimations() {
+function introAnimations() {
   window.addEventListener(
     'load',
     animateGsap('.logo', '.title', 'label', '.info', '.show', '.register')
   );
 }
+// TODO buttons to reveal password
+function validityCheck() {
+  // cache DOM
+  const form = document.querySelector('form');
+  const allInputs = form.querySelectorAll('input');
+  const registerBtn = form.querySelector('.register');
+  // event listeners
+  allInputs.forEach((input) => input.addEventListener('input', validate));
+  registerBtn.addEventListener('click', validateSelect);
+  form.addEventListener('submit', preventSubmission);
+}
+
+export { introAnimations, validityCheck };
