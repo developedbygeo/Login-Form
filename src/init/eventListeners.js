a9d7af5e2c9452a3dbc0f92cea679c5605f1f67c
georgiosan <georgiosanagnostou@hotmail.com>
2021-10-12 21:12:31 +0300

    Added eventlisteners for the form, for the successBtn and imported the respective functions

--- src/init/eventListeners.js
+++ src/init/eventListeners.js
@@ -1,5 +1,7 @@
 import animateGsap from '../utils/animation';
 import { validate, validateSelect, preventSubmission } from './validate';
+import { hideDiv } from '../utils/showDiv';
+import showPassword from '../utils/showPass';
 
 function introAnimations() {
   window.addEventListener(
@@ -12,11 +14,17 @@ function validityCheck() {
   // cache DOM
   const form = document.querySelector('form');
   const allInputs = form.querySelectorAll('input');
+  const allShowPasswordButtons = form.querySelectorAll('.show');
   const registerBtn = form.querySelector('.register');
+  const successBtn = document.querySelector('.close');
   // event listeners
   allInputs.forEach((input) => input.addEventListener('input', validate));
+  allShowPasswordButtons.forEach((button) =>
+    button.addEventListener('click', showPassword)
+  );
   registerBtn.addEventListener('click', validateSelect);
   form.addEventListener('submit', preventSubmission);
+  successBtn.addEventListener('click', hideDiv);
 }
 
 export { introAnimations, validityCheck };
