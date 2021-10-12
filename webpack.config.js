6d1355c2c5137d234d83569dcbe6e438f8409025
georgiosan <georgiosanagnostou@hotmail.com>
2021-10-12 18:20:59 +0300

    Adding babel as a dependency / config

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
-const path = require("path");
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  mode: "development",
+  mode: 'development',
   entry: {
-    main: "./src/index.js",
+    main: './src/index.js',
   },
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   devServer: {
     hot: true,
     port: 3000,
@@ -17,12 +17,12 @@ module.exports = {
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new CopyWebpackPlugin({
-      patterns: [{ from: "./src/img/", to: "./img" }],
+      patterns: [{ from: './src/img/', to: './img' }],
     }),
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
       inject: true,
-      title: "Login Form",
+      title: 'Login Form',
     }),
     new MiniCssExtractPlugin(),
   ],
@@ -30,22 +30,34 @@ module.exports = {
     rules: [
       {
         test: /\.(s(a|c)ss)$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(png|svg|jpeg|gif)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread'],
+            cacheDirectory: true,
+          },
+        },
       },
     ],
   },
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
-    assetModuleFilename: "assets/[name]-[hash:3].[ext][query]",
+    assetModuleFilename: 'assets/[name]-[hash:3].[ext][query]',
   },
 };
