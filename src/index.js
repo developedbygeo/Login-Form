e796d1d58aa81b35e612a7c5613127c8393698ca
georgiosan <georgiosanagnostou@hotmail.com>
2021-10-13 11:27:23 +0300

    Adding the main js entry point

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './styles/style.scss';
+// eslint-disable-next-line no-unused-vars
 import fontawesome from '../node_modules/@fortawesome/fontawesome-free/js/all';
-import introAnimations from './init/eventListeners';
+import { introAnimations, validityCheck } from './init/eventListeners';
 
 introAnimations();
+validityCheck();
